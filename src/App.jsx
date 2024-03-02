b8907f45a344736bee53f5be1c168a9bfbd9b166
Abhishek13 <abhishek7109501@gmail.com>
2024-03-03 02:35:31 +0530

    navbar and sidebar completed

--- src/App.jsx
+++ src/App.jsx
@@ -37,9 +37,9 @@ function App() {
               <div className={
                 `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72':'flex-2'}`
               }>
-                {/* <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+                <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                    <Navbar />
-                </div> */}
+                </div>
               </div>
                <div>
                 <Routes>
--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -2,6 +2,7 @@ import React,{useContext,createContext,useState} from 'react';
 
 const StateContext = createContext();
 
+
 const initialState = {
   chat:false,
   cart:false,
@@ -11,8 +12,13 @@ const initialState = {
 
 function ContextProvider({children}) {
     const [activeMenu,setActiveMenu] = useState(true);
+    const [isClicked,setIsClicked] = useState(initialState);
+    const [screenSize,setScreenSize] = useState(undefined);
+    const handleClick = (clicked) =>{
+      setIsClicked({...initialState,[clicked]:true})
+    }
   return (
-    <StateContext.Provider value={{activeMenu,setActiveMenu}}>
+    <StateContext.Provider value={{activeMenu,setActiveMenu,isClicked,setIsClicked,handleClick,screenSize,setScreenSize}}>
       {children}
     </StateContext.Provider>
   );
